469394065c9bdfc2c27ebf8f6393140e6b753bc2
Reuben Ninan <reubenmn@red.eecs.yorku.ca>
2020-01-21 19:56:11 -0500

    Lab2 complete

---
+++ Lab2/Dio.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+x=$1
+y=$2
+z=$3
+w=$4
+
+for (( i = 1;  i <= $x; i++ ))
+do
+	for (( j = 1; j <= $y; j++ ))
+	do
+		for (( k = 1; k <= $z; k++ ))
+		do
+			for (( l = 1; l <= $w; l++))
+			do
+				first=$((i**3 + j**3))
+				second=$((k**3 + l**3))
+				#echo $first $second
+				if (( $first == $second ))
+				then
+					#echo $first $second
+					echo "x= "$i", y= "$j", z= "$k", w= "$l
+				fi
+			done
+		done
+	done
+done
+
+
+
---
+++ Lab2/sh1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+dir=$1
+filename=$2
+path="$dir/$filename"
+
+#echo $path
+
+if [[ $# -ne 2 ]]
+then
+	echo "usage: sh1.sh dir file" && exit 1
+fi
+
+if [[ ! -d $dir ]]
+then 
+	echo "$dir is not a directory" && exit 1
+fi
+
+if [[ -d $2 ]]
+then
+	echo "$filename is not a regular file" && exit 1
+fi
+
+if [[ -f $path ]]
+then
+	echo "file $2 is in $dir" && exit 1
+else 
+	echo "file $2 is not in $dir" && exit 1
+fi
